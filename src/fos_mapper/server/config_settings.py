6efbe753b543311b80645c827f6dbfaae6c3d3c0
sotiriskot9 <sotiriskot9@gmail.com>
2024-05-24 14:39:41 +0300

    refactored config_settings to account for the taxonomy versioning

--- src/fos_mapper/server/config_settings.py
+++ src/fos_mapper/server/config_settings.py
@@ -10,7 +10,9 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     es_passwd: str
-    ca_certs_path: str
+    ca_certs_path: str | None = None
     device: str = "cuda"
     es_host: str = "localhost"
     es_port: str = "9200"
+    index_name: str = "fos_taxonomy_01_embed"
+    fos_taxonomy_version: str = "v0.1.1"
